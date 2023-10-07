32a99c884752f1496021087a5367763ab99964d8
Shree <rohitfzd9897@gmail.com>
2023-10-07 20:18:53 +0530

    intitial commet git demo 1

---
+++ src/demo_veriable/demo_variable1.java
@@ -0,0 +1,40 @@
+package demo_veriable;
+
+public class demo_variable1 {
+
+    public static void main(String []agrs){
+        String bookName;
+        bookName="RAMAYANA";
+        System.out.println("Printing variable data; " +bookName);
+
+        int pages;
+         pages=1001;
+         System.out.println("Printing pages;"+pages);
+
+         double price;
+         price=456.50;
+         System.out.println("Printing price;"+price);
+
+         String writerName;
+         writerName="TULSI DAS";
+         System.out.println("Printing writerName; "+writerName);
+         System.out.println( "     This is the most popular in INDIA");
+
+
+         String cityName;
+         cityName="UTTAR PRADESH";
+         System.out.println("Printing cityName;"+cityName);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
